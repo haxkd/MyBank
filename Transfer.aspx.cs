2a24a756aceacbb99aaa9b47a888de27d6986c8d
Abul Hasan <Abulhax@gmail.com>
2024-01-08 23:48:50 +0530

    Update Transfer.aspx.cs

--- Transfer.aspx.cs
+++ Transfer.aspx.cs
@@ -24,59 +24,44 @@ namespace MyBank
             if (status == "freeze")
             {
                 Response.Write("<script>alert('account is freeze, cant transfer amount....!')</script>");
-
                 HtmlMeta meta = new HtmlMeta();
                 meta.HttpEquiv = "Refresh";
                 meta.Content = "0;url=Dashboard.aspx";
                 this.Page.Controls.Add(meta);
             }
-
         }
 
         protected void btn_Click(object sender, EventArgs e)
         {
             string UserId = Session["UserId"].ToString();
-
             string toAccountNo = accountNo.Value;
-
             if (toAccountNo==UserId)
             {
                 Response.Write("<script>alert('you cant transfer on your account....!')</script>");
                 return;
             }
-
-
             if (UserLogic.getCutsomer(toAccountNo).Rows.Count == 0) {
                 Response.Write("<script>alert('invalid account number....!')</script>");
                 return;
-            }
-            
+            }            
             DataRow toAccountRow = UserLogic.getCutsomer(toAccountNo).Rows[0];
-
             int transferAmount = int.Parse(amount.Value);
-
             DataRow user = UserLogic.getCutsomer(UserId).Rows[0];
-
-
             int fromBalance = int.Parse(user["balance"].ToString());
             int minAmount = int.Parse(user["minAmount"].ToString());
             int maxAmount = int.Parse(user["maxAmount"].ToString());
-
             if ((fromBalance - transferAmount) < minAmount)
             {
                 Response.Write("<script>alert('insufficient balance....!')</script>");
                 return;
             }
-
             if (transferAmount > maxAmount)
             {
                 Response.Write($"<script>alert('yout maximum transfer limit is {maxAmount} ....!')</script>");
                 return;
             }
-
             int toAmount = int.Parse(toAccountRow["balance"].ToString()) + transferAmount;
             int fromAmount = fromBalance - transferAmount;
-
             int x = UserLogic.transferAmount(transferAmount, toAmount, fromAmount, toAccountNo, UserId);
             if (x > -1)
             {
@@ -86,8 +71,6 @@ namespace MyBank
             {
                 Response.Write("<script>alert('transfer failed....!')</script>");
             }
-
-
         }
     }
 }
