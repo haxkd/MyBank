0eb2954bbb37e32c5bceb502bf1074de6c013314
Abul Hasan <Abulhax@gmail.com>
2024-01-08 23:47:59 +0530

    Update Register.aspx.cs

--- Register.aspx.cs
+++ Register.aspx.cs
@@ -13,12 +13,10 @@ namespace MyBank
     public partial class Register : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-            
+        {            
             if (!IsPostBack)
             {
                 DataTable dataTable = AdminLogic.GetBranches();
-
                 branches.DataSource = dataTable;
                 branches.DataValueField = "id";
                 branches.DataTextField = "name";
@@ -36,17 +34,14 @@ namespace MyBank
             var ps = password.Value;
             var ad = address.Value;
             var branch = branches.SelectedValue;
-
             if (UserLogic.checkCustomer(em).Rows.Count!=0)
             {
                 Response.Write("<script>alert('customer already exist....!')</script>");
                 return;
             }
-
             int userid = UserLogic.addCustomer(nm, ag, gn, em, ps, ad, branch);
             DataRow row = AdminLogic.getBranch(branch).Rows[0];
             string branchData = $"{ row["code"]} | {row["name"]} | {row["address"]}";
-
             new SendMails().registerMail(userid.ToString(), nm, gn, em, ad, branchData);
             Response.Write("<script>alert('customer registeration successfully....!')</script>");          
         }
